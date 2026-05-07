c8c85d00cbe6fed072c4c671587347a055307302
Ashutosh raj <ashu7890raj@gmail.com>
2026-05-07 16:38:43 +0530

    Add preview configuration to Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+
+    allowedHosts: [
+      'ingenious-flow-production-ef5c.up.railway.app',
+    ],
+  },
+});
