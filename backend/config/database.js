891eb3e6548fc94c71d198741c524dd87b0853b7
Ashutosh <ashu7890raj@gmail.com>
2026-05-07 13:56:55 +0530

    update database.js for mysql connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,9 +7,11 @@ const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
+  
   {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306, // ✅ MySQL port
+    
+    host: process.env.DB_HOST ,
+    port: process.env.DB_PORT , // ✅ MySQL port
     dialect: 'mysql', // ✅ FIXED
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
     pool: {
