9a4ffc5b8cd5673cc3fdbce1e13d5f8669ed69ba
Ashutosh raj <ashu7890raj@gmail.com>
2026-05-07 16:47:47 +0530

    Add CORS methods and allowed headers

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,8 @@ app.use(
       process.env.FRONTEND_URL,
     ].filter(Boolean),
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
 
